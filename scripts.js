91c699fd9af88422cb7941f542a24f6f8ccd003d
Bimbola Lasisi <78049244+blasisi@users.noreply.github.com>
2022-10-10 14:36:17 +0100

    code refactor and styles

--- scripts.js
+++ scripts.js
@@ -1,24 +1,26 @@
 const quoteText = document.querySelector(".quote");
-// const authorName =document.querySelector("author");
+const authorName =document.querySelector("author");
 quoteBtn = document.getElementById("btn");
 // console.log(quoteBtn)
 
 async function randomQuoteSelect() {
   quoteText.innerText = 'Loading....'
-
-  const quotes = await fetch("./quoteApi.json")
-    .then(res => res.json())
+//Get quotes from Api
+  const quotes = await fetch("./quoteApi.json")//set fetch quest inside respon request
+    .then(res => res.json())// get json as a reston, turn into json option 
     .then(result => { //fetch return  a promiseresorces from the api
       return result
 
     });
 
-  if (quoteText > 0) {
+  if (quoteText && authorName > 0) {
     console.log("quoteText is greater 0")
 
   }
+  //show new qupte
   console.log(quotes)
-  let arrayIndex = Math.floor(Math.random() * quotes.length);
+  let arrayIndex = Math.floor(Math.random() * quotes.length);//wrapped  math floor into math random fuction to return to whole interger. this allow o pic a random quote from quotetext
+  // let arrayIndex = quoteApi[Math.floor(Math.random() * quoteApi.length)];
   console.log(arrayIndex);
   // console.log(quotes[arrayIndex].quote)
   quoteText.innerText = quotes[arrayIndex].quote
